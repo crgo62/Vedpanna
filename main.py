49357aec71b3ee7f4b7084e3a97895b71970496e
Pavilion <christer.gransson@telia.com>
2021-12-27 17:01:17 +0100

    Tk ver 2

--- main.py
+++ main.py
@@ -9,9 +9,16 @@ def click():
         definition = "Sorry there is no word like that please try again"
     output.insert(END, definition)
 
+def close_window():
+    window.destroy()
+    exit()
+
 
 window = Tk()
+window.geometry('600x400')
 window.title("My Comp")
+
+
 
 Label( window, text="Enter the word:", fg="black", font="non 12 bold") .grid(row=1, column=0)
 
@@ -29,6 +36,10 @@ my_compdictionary = {
     'algorithm': 'Step by step instruction to complete a task', 'bug': 'Piece of code that cause a program to fail'
     }
 
+Button(window, text="Close", width=6, command=close_window) .grid(row=7, column=0)
+
+c = Canvas( window, height=50, width=50, bg="blue")
+c.grid(row=8, column=1)
 
 window.mainloop()
 
